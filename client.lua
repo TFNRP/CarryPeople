35887177510d0941b22d0a45f2c8ed66579d7ead
Hagen <hagen@paw.bot>
2021-10-18 23:04:47 +0100

    refactor: linting and general clean up

--- client.lua
+++ client.lua
@@ -1,93 +1,90 @@
 local carry = {
-	InProgress = false,
-	targetSrc = -1,
-	type = '',
+  carrying = false,
+  serverId = -1,
+  type = '',
 }
 
 local function GetClosestPlayer(radius)
-    local players = GetActivePlayers()
-    local closestDistance = -1
-    local closestPlayer = -1
-    local playerPed = PlayerPedId()
-    local playerCoords = GetEntityCoords(playerPed)
+  local closestDistance = -1
+  local closestPlayer = -1
+  local ped = PlayerPedId()
 
-    for _,playerId in ipairs(players) do
-        local targetPed = GetPlayerPed(playerId)
-        if targetPed ~= playerPed then
-            local targetCoords = GetEntityCoords(targetPed)
-            local distance = #(targetCoords-playerCoords)
-            if closestDistance == -1 or closestDistance > distance then
-                closestPlayer = playerId
-                closestDistance = distance
-            end
-        end
+  for _, playerId in ipairs(GetActivePlayers()) do
+    local playerPed = GetPlayerPed(playerId)
+    if playerPed ~= ped and DoesEntityExist(playerPed) then
+      local distance = #(GetEntityCoords(playerPed) - GetEntityCoords(ped))
+      if closestDistance == -1 or closestDistance > distance then
+        closestPlayer = playerId
+        closestDistance = distance
+      end
     end
-	if closestDistance ~= -1 and closestDistance <= radius then
-		return closestPlayer
-	else
-		return nil
-	end
+  end
+  if closestDistance ~= -1 and closestDistance <= radius then
+    return closestPlayer
+  else
+    return nil
+  end
 end
 
-local function ensureAnimDict(animDict)
-    if not HasAnimDictLoaded(animDict) then
-        RequestAnimDict(animDict)
-        while not HasAnimDictLoaded(animDict) do
-            Wait(0)
-        end        
+local function ensureAnimDict(anim)
+  if not HasAnimDictLoaded(anim) then
+    RequestAnimDict(anim)
+    while not HasAnimDictLoaded(anim) do
+      Wait(0)
     end
-    return animDict
+  end
+  return anim
 end
 
-RegisterCommand('carry',function(source, args)
-	if not carry.InProgress then
-		local closestPlayer = GetClosestPlayer(3)
-		if closestPlayer then
-			local targetSrc = GetPlayerServerId(closestPlayer)
-			if targetSrc ~= -1 then
-				carry.InProgress = true
-				carry.targetSrc = targetSrc
-				TriggerServerEvent('CarryPeople:sync',targetSrc)
-				ensureAnimDict('missfinale_c2mcs_1')
-				carry.type = 'carrying'
-			end
-		end
-	else
-		carry.InProgress = false
-		ClearPedSecondaryTask(PlayerPedId())
-		DetachEntity(PlayerPedId(), true, false)
-		TriggerServerEvent('CarryPeople:stop',carry.targetSrc)
-		carry.targetSrc = 0
-	end
-end,false)
+RegisterCommand('carry', function()
+  if not carry.carrying then
+    local closestPlayer = GetClosestPlayer(3)
+    if closestPlayer then
+      local serverId = GetPlayerServerId(closestPlayer)
+      if serverId ~= -1 then
+        carry.carrying = true
+        carry.serverId = serverId
+        TriggerServerEvent('CarryPeople:sync', serverId)
+        ensureAnimDict('missfinale_c2mcs_1')
+        carry.type = 'carrying'
+      end
+    end
+  else
+    carry.carrying = false
+    ClearPedSecondaryTask(PlayerPedId())
+    DetachEntity(PlayerPedId(), true, false)
+    TriggerServerEvent('CarryPeople:stop', carry.serverId)
+    carry.serverId = 0
+  end
+end, false)
 
 RegisterNetEvent('CarryPeople:syncTarget', function(targetSrc)
-	local targetPed = GetPlayerPed(GetPlayerFromServerId(targetSrc))
-	carry.InProgress = true
-	ensureAnimDict('nm')
-	AttachEntityToEntity(PlayerPedId(), targetPed, 0, .27, .15, .63, 0.5, 0.5, 180, false, false, false, false, 2, false)
-	carry.type = 'beingcarried'
+  local targetPed = GetPlayerPed(GetPlayerFromServerId(targetSrc))
+  carry.carrying = true
+  ensureAnimDict('nm')
+  AttachEntityToEntity(PlayerPedId(), targetPed, 0, .27, .15, .63, .5, .5, 180, false, false, false, false, 2, false)
+  carry.type = 'beingcarried'
 end)
 
 RegisterNetEvent('CarryPeople:cl_stop', function()
-	carry.InProgress = false
-	ClearPedSecondaryTask(PlayerPedId())
-	DetachEntity(PlayerPedId(), true, false)
+  carry.carrying = false
+  ClearPedSecondaryTask(PlayerPedId())
+  DetachEntity(PlayerPedId(), true, false)
 end)
 
 Citizen.CreateThread(function()
-	while true do
-		if carry.InProgress then
-			if carry.type == 'beingcarried' then
-				if not IsEntityPlayingAnim(PlayerPedId(), 'nm', 'firemans_carry', 3) then
-					TaskPlayAnim(PlayerPedId(), 'nm', 'firemans_carry', 8.0, -8.0, 100000, 33, 0, false, false, false)
-				end
-			elseif carry.type == 'carrying' then
-				if not IsEntityPlayingAnim(PlayerPedId(), 'missfinale_c2mcs_1', 'fin_c2_mcs_1_camman', 3) then
-					TaskPlayAnim(PlayerPedId(), 'missfinale_c2mcs_1', 'fin_c2_mcs_1_camman', 8.0, -8.0, 100000, 49, 0, false, false, false)
-				end
-			end
-		end
-		Wait(0)
-	end
+  while true do
+    if carry.carrying then
+      if carry.type == 'beingcarried' then
+        if not IsEntityPlayingAnim(PlayerPedId(), 'nm', 'firemans_carry', 3) then
+          TaskPlayAnim(PlayerPedId(), 'nm', 'firemans_carry', 8.0, -8.0, 100000, 33, 0, false, false, false)
+        end
+      elseif carry.type == 'carrying' then
+        if not IsEntityPlayingAnim(PlayerPedId(), 'missfinale_c2mcs_1', 'fin_c2_mcs_1_camman', 3) then
+          TaskPlayAnim(PlayerPedId(), 'missfinale_c2mcs_1', 'fin_c2_mcs_1_camman', 8.0, -8.0, 100000, 49, 0, false, false, false)
+        end
+      end
+    end
+    Wait(0)
+  end
 end)
--- server.lua
+++ server.lua
@@ -2,45 +2,38 @@ local carrying = {}
 local carried = {}
 
 RegisterServerEvent('CarryPeople:sync')
-AddEventHandler('CarryPeople:sync', function(targetSrc)
-	local source = source
-	local sourcePed = GetPlayerPed(source)
-   	local sourceCoords = GetEntityCoords(sourcePed)
-	local targetPed = GetPlayerPed(targetSrc)
-        local targetCoords = GetEntityCoords(targetPed)
-	if #(sourceCoords - targetCoords) <= 3.0 then 
-		TriggerClientEvent('CarryPeople:syncTarget', targetSrc, source)
-		carrying[source] = targetSrc
-		carried[targetSrc] = source
-	end
+AddEventHandler('CarryPeople:sync', function(serverId)
+  if #(
+    GetEntityCoords(GetPlayerPed(source)) - GetEntityCoords(GetPlayerPed(serverId))
+  ) <= 3.0 then
+    TriggerClientEvent('CarryPeople:syncTarget', serverId, source)
+    carrying[source] = serverId
+    carried[serverId] = source
+  end
 end)
 
-RegisterServerEvent('CarryPeople:stop', function(targetSrc)
-	local source = source
-
-	if carrying[source] then
-		TriggerClientEvent('CarryPeople:cl_stop', targetSrc)
-		carrying[source] = nil
-		carried[targetSrc] = nil
-	elseif carried[source] then
-		TriggerClientEvent('CarryPeople:cl_stop', carried[source])			
-		carrying[carried[source]] = nil
-		carried[source] = nil
-	end
+RegisterServerEvent('CarryPeople:stop', function(serverId)
+  if carrying[source] then
+    TriggerClientEvent('CarryPeople:cl_stop', serverId)
+    carrying[source] = nil
+    carried[serverId] = nil
+  elseif carried[source] then
+    TriggerClientEvent('CarryPeople:cl_stop', carried[source])
+    carrying[carried[source]] = nil
+    carried[source] = nil
+  end
 end)
 
-AddEventHandler('playerDropped', function(reason)
-	local source = source
-	
-	if carrying[source] then
-		TriggerClientEvent('CarryPeople:cl_stop', carrying[source])
-		carried[carrying[source]] = nil
-		carrying[source] = nil
-	end
+AddEventHandler('playerDropped', function()
+  if carrying[source] then
+    TriggerClientEvent('CarryPeople:cl_stop', carrying[source])
+    carried[carrying[source]] = nil
+    carrying[source] = nil
+  end
 
-	if carried[source] then
-		TriggerClientEvent('CarryPeople:cl_stop', carried[source])
-		carrying[carried[source]] = nil
-		carried[source] = nil
-	end
+  if carried[source] then
+    TriggerClientEvent('CarryPeople:cl_stop', carried[source])
+    carrying[carried[source]] = nil
+    carried[source] = nil
+  end
 end)
